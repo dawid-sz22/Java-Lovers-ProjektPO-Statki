99b39b3cbf14449ded5c0104be3607cb7633c474
JonStuPwr <263898@student.pwr.edu.pl>
2022-05-24 22:29:35 +0200

    Update Main.java

--- Statki/src/com/company/Main.java
+++ Statki/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         Sea balticSea = new Sea(20,20,2,2,3,1,2,3);
         balticSea.print();
 
